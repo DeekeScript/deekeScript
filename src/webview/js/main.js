5110a3a10fa1690b9c94ae005f3d9b46cb722f02
jiangqiao <miniphper@gmail.com>
2025-08-22 18:02:58 +0800

    1

--- src/webview/js/main.js
+++ src/webview/js/main.js
@@ -1,4 +1,12 @@
 
-function btn_func(_this){
-    Deeke.toast(_this.innerHTML);
+function btn_func(_this) {
+    let json = {
+        name: "DeekeScript",
+        age: 2,
+    }
+
+    DeekeScriptWebView.run((json) => {
+        console.log(json);
+        console.log(json.name);
+    }, json);
 }
