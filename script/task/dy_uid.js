39912011cd9de25940d64002a2011f8a400bff35
jiangqiao <1062241667@qq.com>
2025-08-29 01:34:37 +0800

    build

---
+++ script/task/dy_uid.js
@@ -0,0 +1,347 @@
+let Common = require('../util/dy/Common');
+let machine = require('../common/machine');
+
+let dy = {
+    debug: false,
+    showUserTag() {
+        let scrollTag = UiSelector().className('android.widget.HorizontalScrollView').scrollable(true).findOne();
+        let userTag;
+        let i = 0;
+        do {
+            userTag = UiSelector().id('android:id/text1').text('用户').isVisibleToUser(true).filter(v => {
+                return v.bounds().left < Device.width() * 0.7;
+            }).findOne();
+            if (userTag) {
+                Common.log('找到用户tab');
+                Common.log(userTag);
+                break;
+            }
+            // scrollTag.scrollForward();
+
+            Gesture.swipe(Device.width() * (0.7 + 0.1 * Math.random()), scrollTag.bounds().centerY() + 20 * Math.random(), scrollTag.bounds().left + Device.width() * (0.2 + 0.1 * Math.random()), scrollTag.bounds().centerY(), 400 + 100 * Math.random());
+            Common.sleep(500 + 500 * Math.random());
+            Common.log('没有找到”用户“tab，滑动');
+        } while (!userTag || i++ < 3);
+        return userTag;
+    },
+
+    fansCountShow(type) {
+        let text = ['不限', '1000以下', '1000-1w', '1w-10w', '10w-100w', '100w以上'][type];
+        let scrollTags = UiSelector(false).className('androidx.recyclerview.widget.RecyclerView').scrollable(true).find();
+        let fansCountTag;
+        let i = 0;
+        do {
+            fansCountTag = UiSelector(false).text(text).isVisibleToUser(true).findOne();
+            if (fansCountTag) {
+                Common.log("找到了fansCountTag");
+                break;
+            }
+            scrollTags[0].scrollForward();
+            Common.sleep(500 + 500 * Math.random());
+            Common.log('没有找到fansCountTag，滑动');
+        } while (!fansCountTag || i++ < 3);
+        return fansCountTag;
+    },
+
+    userTypeShow(type) {
+        let text = ['不限', '普通用户', '企业认证', '个人认证'][type];
+        let scrollTags = UiSelector(false).className('androidx.recyclerview.widget.RecyclerView').scrollable(true).find();
+        let userTypeTag;
+        let i = 0;
+        do {
+            userTypeTag = UiSelector(false).text(text).isVisibleToUser(true).findOne();
+            if (userTypeTag) {
+                Common.log('找到了userTypeTag');
+                break;
+            }
+            scrollTags[1].scrollForward();
+            Common.sleep(500 + 500 * Math.random());
+            Common.log('没有找到userTypeTag，滑动');
+        } while (!userTypeTag || i++ < 3);
+        return userTypeTag;
+    },
+    searchKeyword(keyword) {
+        let searchTag = UiSelector().desc('搜索').isVisibleToUser(true).findOne();
+        if (searchTag) {
+            Common.log('找到了搜索按钮');
+            Common.click(searchTag, 0.2);
+            Common.sleep(1500 + 1000 * Math.random());
+        }
+
+        let iptTag = UiSelector().className('android.widget.EditText').isVisibleToUser(true).editable(true).findOne();
+        if (iptTag) {
+            Common.log('找到了输入框');
+            Common.click(iptTag, 0.25);
+            Common.sleep(1500 + 1000 * Math.random());
+        }
+
+        iptTag = UiSelector().className('android.widget.EditText').isVisibleToUser(true).editable(true).findOne();
+        if (iptTag) {
+            Common.log('找到了输入框2');
+            iptTag.setText(keyword);
+            Common.sleep(1500 + 1000 * Math.random());
+        }
+
+        let searchBtnTag = UiSelector().desc('搜索').isVisibleToUser(true).findOne();
+        if (searchBtnTag) {
+            Common.log('找到了搜索按钮2');
+            Common.click(searchBtnTag, 0.2);
+            Common.sleep(2000 + 2000 * Math.random());
+        }
+
+        let userTag = this.showUserTag();
+        if (userTag) {
+            Common.log('找到了用户tab');
+            Common.click(userTag, 0.2);
+            Common.sleep(2000 + 2000 * Math.random());
+        }
+
+        let filterTag = UiSelector().desc('筛选，按钮').isVisibleToUser(true).findOne();
+        if (filterTag) {
+            Common.log('找到了筛选按钮');
+            Common.click(filterTag, 0.2);
+            Common.sleep(2000 + 2000 * Math.random());
+            return true;
+        }
+
+        return false;
+    },
+    searchUser(keyword, config) {
+        if (!this.searchKeyword(keyword)) {
+            throw new Error('没有找到搜索内容');
+        }
+
+        let fansUserTag = this.fansCountShow(config.fansType);
+        if (fansUserTag) {
+            Common.log('找到了fansUserTag');
+            Common.click(fansUserTag, 0.2);
+            Common.sleep(2000 + 2000 * Math.random());
+        }
+
+        let userTypeTag = this.userTypeShow(config.userType);
+        if (userTypeTag) {
+            Common.log('找到了userTypeTag');
+            Common.click(userTypeTag, 0.2);
+            Common.sleep(2000 + 2000 * Math.random());
+            Gesture.click(Device.width() * (0.2 + 0.6 * Math.random()), Device.height() * (0.6 + 0.3 * Math.random()));
+            Common.sleep(1000 + 1000 * Math.random());
+        }
+    },
+
+    privateMsg(msg) {
+        let moreTag = UiSelector().descContains('更多').filter(v => {
+            // @ts-ignore
+            return v.desc() == '更多' && v.getHintText() == '按钮' && v.bounds().top < Device.height() / 4;
+        }).isVisibleToUser(true).findOne();
+        if (!moreTag) {
+            Common.log('没有更多按钮');
+            return false;
+        }
+        Common.click(moreTag, 0.3);
+        Common.sleep(1000 + 1000 * Math.random());
+
+        let sendMsgTag = UiSelector().className('android.widget.TextView').textContains('发私信').filter(v => {
+            return v.bounds().top > Device.height() / 2;
+        }).isVisibleToUser(true).findOne();
+        if (!sendMsgTag) {
+            Common.log("没有找到发送私信按钮");
+            return false;
+        }
+
+        Common.click(sendMsgTag, 0.2);
+        Common.sleep(2000 + 1000 * Math.random());
+
+        let iptTag = UiSelector().className('android.widget.EditText').filter(v => {
+            return v.isEditable();
+        }).isVisibleToUser(true).findOne();
+        if (!iptTag) {
+            Common.log("没有找到发送私信输入框");
+            return false;
+        }
+
+        Common.click(iptTag, 0.2);
+        Common.sleep(1000 + 1000 * Math.random());
+
+        iptTag = UiSelector().className('android.widget.EditText').filter(v => {
+            // @ts-ignore
+            return v.isEditable() && v.isFocused();
+        }).isVisibleToUser(true).findOne();
+        if (!iptTag) {
+            Common.log('没有找到点击后的输入框');
+            return true;
+        }
+
+        iptTag.setText(msg);
+        Common.sleep(1000 + 1000 * Math.random());
+        let btnTag = UiSelector().className('android.widget.ImageView').desc('发送').isVisibleToUser(true).findOne();
+        if (!btnTag) {
+            Common.log('没有找到发送按钮');
+            return false;
+        }
+        if (this.debug) {
+            Common.sleep(1000 + 1000 * Math.random());
+            Common.back(2);
+            Common.log('消息已发送', msg);
+            return true;
+        }
+        Common.click(btnTag, 0.25);
+        Common.sleep(1000 + 1000 * Math.random());
+        Common.back(2);
+        Common.log('消息已发送', msg);
+        return true;
+    },
+
+    userListForward() {
+        let tag = UiSelector().className('androidx.recyclerview.widget.RecyclerView').scrollable(true).isVisibleToUser(true).filter(v => {
+            return v.bounds().width() >= Device.width() - 10 && v.bounds().height() > Device.height() * 0.7;
+        }).findOne();
+        Common.log('滑动列表');
+        return tag.scrollForward();
+    }
+}
+
+let task = {
+    startTime: Date.parse(new Date()) / 1000,
+    getMsg(type) {
+        return machine.getMsg(type) || false;//永远不会结束
+    },
+
+    getConfig() {
+        //console.log(Storage.getString('toker_dy_uids'));
+        return {
+            keywords: Storage.getString('toker_dy_keywords'),
+            address: Storage.getString('toker_dy_address'),
+            shops: Storage.getString('toker_dy_shop'),
+            second: Storage.getArray('toker_dy_second'),
+            privateCount: Storage.getInteger('toker_dy_private_count'),
+            fansType: Storage.getString("toker_dy_fans_count"),
+            userType: Storage.getString("toker_dy_type"),
+            runMinute: Storage.getInteger('toker_dy_run_minute'),
+            sleepMinute: Storage.getInteger('toker_dy_sleep_minute'),
+        }
+    },
+
+    ops: [],
+
+    opUserList(config) {
+        let tag = UiSelector().text('搜索结果为空').isVisibleToUser(true).findOne();
+        if (tag) {
+            return false;
+        }
+
+        while (true) {
+            let userListTag = UiSelector().className('com.lynx.tasm.behavior.ui.LynxFlattenUI').descContains('粉丝:').isVisibleToUser(true).find();
+            for (let i in userListTag) {
+                //检测是不是在列表页，不是则返回
+                let rp = 0;
+                while (!UiSelector().desc('搜索').isVisibleToUser(true).findOne() || !UiSelector().id('android:id/text1').text('用户').isVisibleToUser(true).findOne()) {
+                    Common.back();
+                    Common.log('不在列表页面，返回');
+                    if (rp++ > 3) {
+                        throw new Error('重新开始');
+                    }
+                    Common.sleep(1000);
+                }
+
+                Common.log(Date.parse(new Date()) / 1000 - this.startTime, config.runMinute * 60);
+                if (Date.parse(new Date()) / 1000 - this.startTime > config.runMinute * 60) {
+                    Common.sleep(config.sleepMinute * 60 * 1000);
+                    this.startTime = Date.parse(new Date()) / 1000;
+                    return 1;
+                }
+
+                let userTag = userListTag[i];
+                let title = userTag.text();
+                if ((config.userType == '2' || config.userType == '3') && title.indexOf('抖音号') != -1) {
+                    Common.sleep(1000 + 1000 * Math.random());
+                    Common.log('带有抖音号的用户');
+                    continue;
+                }
+                if (this.ops.includes(title)) {
+                    Common.sleep(1000 + 1000 * Math.random());
+                    Common.log('重复用户');
+                    continue;
+                }
+                Common.click(userTag, 0.2);
+                if (UiSelector().desc('搜索').isVisibleToUser(true).findOne() && UiSelector().id('android:id/text1').text('用户').isVisibleToUser(true).findOne()) {
+                    Common.log('没有进入用户主页');
+                    Common.sleep(1000 + 1000 * Math.random());
+                    continue;
+                }
+                Common.sleep(3000 + 3000 * Math.random());
+                if (!UiSelector().text('本店团购').isVisibleToUser(true).findOne()) {
+                    Common.log('没有团购');
+                    Common.back();
+                    Common.sleep(500 + 1000 * Math.random());
+                    continue;
+                }
+
+                let tags = UiSelector().filter(v => {
+                    return !!v.desc();
+                }).isVisibleToUser(true).find();
+                let douyinAccount = tags[2].desc();
+                if (Storage.getBoolean('dy_uid_' + douyinAccount)) {
+                    Common.back();
+                    Common.log('用户已存在');
+                    Common.sleep(1000 + 1000 * Math.random());
+                    continue;
+                }
+
+                dy.privateMsg(this.getMsg(1));
+                // Common.back();这里不用返回
+                Common.log('返回到列表');
+                Storage.putBoolean('dy_uid_' + douyinAccount, true);
+                if (--config.privateCount <= 0) {
+                    return true;
+                }
+
+                this.ops.push(title);
+                Common.sleep((config + config * Math.random()) * 1000);
+            }
+            dy.userListForward();
+            Common.sleep(2000 + 1200 * Math.random());
+        }
+    },
+
+    getKeyword(config) {
+        if (config.keywords.trim() != '') {
+            let keywords = config.keywords.split("\n");
+            let keywordsIndex = Math.floor(Math.random() * keywords.length);
+            return keywords[keywordsIndex].replace("\r", "");
+        }
+        let address = config.address.split("\n");
+        let shops = config.shops.split("\n");
+        let addressIndex = Math.floor(Math.random() * address.length);
+        let shopIndex = Math.floor(Math.random() * shops.length);
+        return address[addressIndex].replace("\r", "") + shops[shopIndex].replace("\r", "");
+    },
+
+    run() {
+        let config = task.getConfig();
+        let keyword = this.getKeyword(config);
+        Common.log('配置', config, keyword);
+        System.toast("即将搜索关键词：" + keyword);
+        System.sleep(2000);
+
+        Common.openApp();
+        dy.searchUser(keyword, config);
+        return this.opUserList(config);
+    }
+}
+
+while (true) {
+    try {
+        let res = task.run();
+        if (res === true) {
+            FloatDialogs.show('任务完成');
+            System.exit();
+        } else if (res === 1) {
+            Common.log('休眠时间');
+            Common.backHomeOnly();
+            continue;
+        }
+    } catch (e) {
+        Common.backHomeOnly();
+        Common.log('出错了', e.stack);
+    }
+}
---
+++ script/task/xhs_uid.js
@@ -0,0 +1,314 @@
+let Common = require('../util/xhs/Common');
+let machine = require('../common/machine');
+let xhs = {
+    getAvatar() {
+        return UiSelector().descContains('头像').isVisibleToUser(true).findOne();
+    },
+
+    intoWork() {
+        let nicknameTag = UiSelector().descContains('作者').isVisibleToUser(true).filter(v => {
+            return v.desc().indexOf('作者') == 0;
+        }).findOne();
+        return nicknameTag.bounds().top < Device.height() / 3;
+    },
+
+    intoVideo() {
+        let nicknameTag = UiSelector().descContains('作者').isVisibleToUser(true).filter(v => {
+            return v.desc().indexOf('作者') == 0;
+        }).findOne();
+        return nicknameTag.bounds().top > Device.height() / 2;
+    },
+
+    getGender() {
+        if (UiSelector().className('android.widget.LinearLayout').descContains('女').isVisibleToUser(true).findOne()) {
+            return '0';
+        }
+
+        if (UiSelector().className('android.widget.LinearLayout').descContains('男').isVisibleToUser(true).findOne()) {
+            return '1';
+        }
+
+        return '2';
+    },
+
+    intoUserVideo() {
+        let videoCountTag = UiSelector().className('android.view.ViewGroup').descContains('笔记').isVisibleToUser(true).findOnce();
+        if (!videoCountTag) {
+            Log.log('没有笔记');
+            return false;
+        }
+
+        if (videoCountTag.desc().includes('已选定')) {
+            Common.click(videoCountTag, 0.2);
+            Common.sleep(1000 + 1000 * Math.random());
+            Log.log('点击笔记');
+        }
+
+        let containerTag = UiSelector().className('android.widget.FrameLayout').filter(v => {
+            return !!v.desc() && (v.desc().indexOf('笔记') == 0 || v.desc().indexOf('视频') == 0);
+        }).isVisibleToUser(true).findOne();
+
+        if (!containerTag) {
+            Log.log('没有笔记或者视频');
+            return false;
+        }
+
+        let type = containerTag.desc().includes('笔记') ? 0 : 1;
+        Common.click(containerTag, 0.3);
+        Log.log('进入' + ['笔记', '视频'][type]);
+        Common.sleep(3000 + 3000 * Math.random());
+        //检查是否进入了视频
+        if (type == 0 && !this.intoWork()) {
+            Log.log('没有进入视频');
+            return false;
+        }
+
+        if (type == 1 && !this.intoVideo()) {
+            Log.log('没有进入视频');
+            return false;
+        }
+
+        Log.log('已进入作品');
+        return type;
+    },
+
+    likeVideo(type) {
+        let likeTag = UiSelector().descContains('点赞').isVisibleToUser(true).findOne();
+        if (!likeTag) {
+            Log.log('没有找到点赞按钮');
+            return false;
+        }
+
+        if (likeTag.desc().includes('已点赞')) {
+            Log.log('历史点赞过了');
+            return true;
+        }
+
+        if (!likeTag.isSelected()) {
+            Common.click(likeTag, 0.3);
+            Common.sleep(1000 + 1000 * Math.random());
+            Log.log('点赞了', likeTag.bounds());
+        }
+
+        Log.log('历史点赞过了2');
+        return true;
+    },
+
+    commentVideo(msg, type) {
+        Log.log('type', type);
+        let CommentCountTag = UiSelector().descContains('评论').className('android.widget.Button').isVisibleToUser(true).findOne();
+        if (!CommentCountTag) {
+            Log.log('没有找到评论按钮');
+            return false;
+        }
+
+        let count = Common.numDeal(CommentCountTag.desc());
+        Log.log('评论数：' + count);
+        Common.click(CommentCountTag, 0.2);
+        Common.sleep(2000 + 1000 * Math.random());
+
+        if (type == 1) {
+            if (count > 0) {
+                Log.log('评论数大于0');
+                let iptTag = UiSelector().className('android.widget.EditText').isVisibleToUser(true).filter(v => {
+                    return v.isEditable();
+                }).findOne();
+                if (!iptTag) {
+                    Log.log('没有找到输入框');
+                    return false;
+                }
+                Common.click(iptTag, 0.2);
+                Common.sleep(1000 + 1000 * Math.random());
+            }
+        } else {
+            if (count > 1) {
+                Common.click(CommentCountTag, 0.2);
+                Common.sleep(2000 + 1000 * Math.random());
+            }
+        }
+
+        let iptTag = UiSelector().className('android.widget.EditText').isVisibleToUser(true).filter(v => {
+            // @ts-ignore
+            return v.isEditable() && v.isFocused();
+        }).findOne();
+        if (!iptTag) {
+            Log.log('没有找到输入框2');
+            return false;
+        }
+        iptTag.setText(msg);
+        Common.sleep(1000 + 1000 * Math.random());
+
+        // @ts-ignore
+        let btnTag = UiSelector().className('android.widget.TextView').text('发送').isVisibleToUser(true).filter(v => {
+            // @ts-ignore
+            return v.text() == '发送' && v.bounds().left > Device.width() * 0.6;
+        }).findOne();
+        if (!btnTag) {
+            Log.log('没有找到发送按钮');
+            return false;
+        }
+        Common.click(btnTag, 0.2);
+        Common.sleep(1000 + 1000 * Math.random());
+        Log.log('评论成功', msg);
+        return true;
+    },
+
+    followUser() {
+        let focusTag = UiSelector().className('android.widget.Button').textContains('关注').isVisibleToUser(true).findOnce();
+        if (!focusTag) {
+            Log.log('没有找到关注按钮');
+            return false;
+        }
+
+        if (focusTag.text() == '已关注' || focusTag.text() == '互相关注') {
+            Log.log('已关注', focusTag.text());
+            return false;
+        }
+
+        Common.click(focusTag, 0.2);
+        Common.sleep(1000 + 1000 * Math.random());
+        Log.log('关注成功');
+        return true;
+    },
+
+    privateMsg(msg) {
+        let sendMsg = UiSelector().className('android.widget.TextView').text('私信').clickable(true).isVisibleToUser(true).findOne() || UiSelector().className('android.widget.TextView').text('发消息').clickable(true).isVisibleToUser(true).findOne();
+        if (!sendMsg) {
+            Log.log('没有私信或者发消息按钮');
+            return false;
+        }
+
+        Common.click(sendMsg, 0.2);
+        Common.sleep(2000 + 2000 * Math.random());
+
+        let iptTag = UiSelector().className('android.widget.EditText').filter(v => {
+            return v.isEditable();
+        }).isVisibleToUser(true).findOne();
+        if (!iptTag) {
+            Log.log("没有找到发送私信输入框");
+            return false;
+        }
+
+        Common.click(iptTag, 0.2);
+        Common.sleep(1000 + 1000 * Math.random());
+
+        iptTag = UiSelector().className('android.widget.EditText').filter(v => {
+            // @ts-ignore
+            return v.isEditable() && v.isFocused();
+        }).isVisibleToUser(true).findOne();
+        if (!iptTag) {
+            Log.log('没有找到点击后的输入框');
+            return true;
+        }
+
+        iptTag.setText(msg);
+        Common.sleep(1000 + 1000 * Math.random());
+        let btnTag = UiSelector().className('android.widget.TextView').text('发送').filter(v => {
+            return v.bounds().top + v.bounds().height() > iptTag.bounds().top;
+        }).clickable(true).isVisibleToUser(true).findOne();
+        if (!btnTag) {
+            Log.log('没有找到发送按钮');
+            return false;
+        }
+        Common.click(btnTag, 0.25);
+        Common.sleep(1000 + 1000 * Math.random());
+        Log.log('消息已发送', msg);
+        return true;
+    }
+}
+
+let task = {
+    getMsg(type) {
+        return machine.getMsg(type) || false;//永远不会结束
+    },
+
+    getConfig() {
+        return {
+            uids: Storage.getString('toker_xhs_uids'),
+            type: Storage.getArray('toker_xhs_type'),//0点赞首作品、1评论首作品、2关注、3私信
+            gender: Storage.getArray('toker_xhs_sex'),
+            second: Storage.getInteger('toker_xhs_second'),
+        }
+    },
+
+    run() {
+        let config = task.getConfig();
+        Log.log('配置', config);
+
+        let uids = config.uids.split("\n");
+        for (let uid of uids) {
+            Log.log('开始操作uid', uid);
+            if (Storage.getBoolean('xhs_uid_' + uid)) {
+                Log.log('uid存在', uid);
+                continue;
+            }
+            App.gotoIntent('xhsdiscover://user/' + uid);
+            Common.sleep(4000 + 2000 * Math.random());
+
+            //看看是否存在用户，不存在则下一个
+            let avatarTag = xhs.getAvatar();
+            if (!avatarTag) {
+                Log.log('用户不存在', uid);
+                continue;
+            }
+
+            let gender = xhs.getGender();
+            if (config.gender.includes(gender)) {
+                Log.log('性别不匹配');
+                continue;
+            }
+
+            if ((config.type.includes(0) || config.type.includes(1))) {
+                let type = xhs.intoUserVideo();
+                if (type !== false) {
+                    if (config.type.includes(0)) {
+                        xhs.likeVideo(type);
+                    }
+
+                    if (config.type.includes(1)) {
+                        xhs.commentVideo(task.getMsg(0), type);
+                        if (type == 1) {
+                            Common.back();
+                            Log.log('视频要返回');
+                        } else {
+                            Log.log('评论了视频，不需要返回');
+                        }
+                    }
+
+                    Common.back();//返回到主页
+                    Log.log('返回到用户主页');
+                    if (!xhs.getAvatar()) {
+                        Log.log('没有返回回来，再操作一次');
+                        Common.sleep(1000);
+                        Common.back();
+                    }
+                }
+            }
+
+            if (config.type.includes(2)) {
+                xhs.followUser();
+            }
+
+            if (config.type.includes(3)) {
+                xhs.privateMsg(task.getMsg(1));
+            }
+
+            Storage.putBoolean('xhs_uid_' + uid, true);
+            Common.backHomeOnly();
+            Common.sleep(config.second / 2 * 1000 + config.second / 2 * 1000 * Math.random());
+        }
+
+        FloatDialogs.show('已执行完毕');
+        System.exit();
+    }
+}
+
+
+while (true) {
+    Common.openApp();
+    try {
+        task.run();
+    } catch (e) {
+        Log.log('出错了', e);
+    }
+}
